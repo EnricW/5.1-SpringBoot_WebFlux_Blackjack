a3e9491d568d82ba9a6786220735ac3891d8f0af
EnricW <reiben815@gmail.com>
2025-02-17 13:28:39 +0100

    Add repository interfaces

--- src/main/java/cat/itacademy/s05/S05/repository/GameRepository.java
+++ src/main/java/cat/itacademy/s05/S05/repository/GameRepository.java
@@ -1,4 +1,11 @@
 package cat.itacademy.s05.S05.repository;
 
-public class GameRepository {
+import cat.itacademy.s05.S05.model.Game;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface GameRepository extends ReactiveMongoRepository<Game, String> {
+    Mono<Game> findById(String id);
 }
--- src/main/java/cat/itacademy/s05/S05/repository/PlayerRepository.java
+++ src/main/java/cat/itacademy/s05/S05/repository/PlayerRepository.java
@@ -1,4 +1,11 @@
 package cat.itacademy.s05.S05.repository;
 
-public class PlayerRepository {
+import cat.itacademy.s05.S05.model.Player;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface PlayerRepository extends ReactiveCrudRepository<Player, Long> {
+    Mono<Player> findByName(String name);
 }
