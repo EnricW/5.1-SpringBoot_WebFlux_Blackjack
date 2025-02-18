555110c5d20fc68516cfc33947d7c0cc2cc37fc8
reiben815 <reiben815@gmail.com>
2025-02-18 19:13:54 +0100

    Add GameState.java

---
+++ src/main/java/cat/itacademy/s05/S05/enums/GameState.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s05.S05.enums;
+
+public enum GameState {
+    NEW,
+    IN_PROGRESS,
+    WAITING_FOR_PLAYER,
+    WAITING_FOR_DEALER,
+    PLAYER_BUSTED,
+    DEALER_BUSTED,
+    ENDED
+}
+
